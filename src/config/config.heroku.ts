31258d530a46f456d12cddf730ef50258521ccc6
Devin Kelly-Collins <devin.kellycollins@sportsengine.com>
2017-06-16 07:18:02 -0500

    Only pass specific options to the redis config.

--- src/config/config.heroku.ts
+++ src/config/config.heroku.ts
@@ -1,11 +1,17 @@
 import * as url from 'redis-url';
 
+const redisUrl = url.parse(process.env.REDIS_URL)
 export const CONFIG = {
   "server": {
     "env": "production"
   },
   "mongo": {},
-  "redis": url.parse(process.env.REDIS_URL)
+  "redis": {
+    host: redisUrl.host,
+    port: redisUrl.port,
+    password: redisUrl.password,
+    db: redisUrl.db
+  }
 }
 
 console.log(CONFIG);
