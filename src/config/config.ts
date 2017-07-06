6acebcf6d847bddd331a455fd25675cd09bd23fd
Devin Kelly-Collins <devin.kellycollins@sportsengine.com>
2017-07-06 07:08:30 -0500

    Using transactions to rollback tests.

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,7 +1,11 @@
+import * as Sequelize from 'sequelize';
+
 export const CONFIG = {
   server: {
     port: 3000
   },
   redis: {},
-  sequelize: {}
+  sequelize: {
+    isolationLevel: Sequelize.Transaction.ISOLATION_LEVELS.READ_UNCOMMITTED
+  }
 }
--- src/spec/_Setup.spec.ts
+++ src/spec/_Setup.spec.ts
@@ -2,11 +2,28 @@ import 'babel-core/register';
 import 'babel-polyfill';
 import {Sequelize} from 'sequelize';
 import container from '../container';
+import {namespace} from '../cls';
 import {TYPES} from '../types';
 import {Config} from '../config';
 
-beforeEach(async () => {
+before(setupDB);
+beforeEach(setupTransaction);
+afterEach(rollbackTransaction);
+
+async function setupDB() {
   const sql = container.get<Sequelize>(TYPES.Models.DataStore);
   await sql.drop();
   await sql.sync({force: true});
-});
+}
+
+async function setupTransaction() {
+  const sql = container.get<Sequelize>(TYPES.Models.DataStore);
+  const transaction = await sql.transaction();
+  namespace.set('transaction', transaction);
+}
+
+function rollbackTransaction() {
+  const transaction = namespace.get('transaction');
+  transaction.rollback();
+  namespace.set('transaction', null);
+}
