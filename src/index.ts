706743fee1a3cda8facc51470e2df653d9c9e7f3
Devin Kelly-Collins <devin.kellycollins@sportsengine.com>
2017-07-10 08:45:44 -0500

    Added support for migrations.

--- src/index.ts
+++ src/index.ts
@@ -5,13 +5,21 @@ import {Config} from './config';
 import container from './container';
 import {TYPES} from './types';
 import {Sequelize} from 'sequelize';
+import * as Umzug from 'umzug';
 
-//TODO: Handle this through migrations.
 console.log('Updating database...');
-const db = container.get<Sequelize>(TYPES.Models.DataStore);
-db.sync({
-    force: true
-  })
+const umzug = new Umzug({
+  storage: 'sequelize',
+  storageOptions: {
+    sequelize: container.get<Sequelize>(TYPES.Models.DataStore)
+  },
+  logging: console.log,
+  migrations: {
+    path: './src/models/migrations'
+  }
+});
+
+umzug.up()
   .then(() => {
     APP.listen(Config.server.port);
     console.log(`Server started on port ${Config.server.port} :)`);
